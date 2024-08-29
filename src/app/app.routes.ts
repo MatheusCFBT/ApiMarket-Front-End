b01af2ceb35b0cc4dad2cbcecb11cd782b54199f
MatheusCFBT <matheusterzini@gmail.com>
2024-08-29 13:00:04 -0300

    Add routes and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './navegacao/home/home.component';
+import { CadastroComponent } from './produtos/cadastro/cadastro.component';
+import { ListaComponent } from './produtos/lista/lista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'cadastrar-produto', component: CadastroComponent},
+    {path: 'lista-produtos', component: ListaComponent}
+];
---
+++ src/app/navegacao/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule, RouterOutlet, RouterLink],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+}
